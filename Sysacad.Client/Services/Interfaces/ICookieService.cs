57862b1b376470e42811c4ab6954b490a77ac65d
AgustinGigena1704 <agustingigena1704@gmail.com>
2026-03-14 10:42:33 -0300

    Muevo cosas a lugar correcto

--- Sysacad.Client/Services/Interfaces/ICookieService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sysacad.Client.Services.Interfaces
-{
-    public interface ICookieService
-    {
-        Task SetCookieAsync(string key, string value, DateTime expires);
-        Task<T?> GetCookieAsync<T>(string key);
-        Task DeleteCookieAsync(string key);
-    }
-}
